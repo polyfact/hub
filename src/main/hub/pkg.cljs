7606e582cb9b7d032a57a9eff5ebe6d7c1f7384c
Victor Forissier <victorforissier@gmail.com>
2023-07-25 20:11:27 +0200

    :zap: Updating lib

--- src/main/hub/pkg.cljs
+++ src/main/hub/pkg.cljs
@@ -1,11 +1,15 @@
 (ns hub.pkg)
 
 (def pkg-list
-  [{:name "polyfact"
-    :description "Polyfact Nodejs SDK: build LLM-based NPM packages."
-    :github-link "https://github.com/polyfact/polyfact-node"
-    :npm-link "https://www.npmjs.com/package/polyfact"}
-   {:name "ai-docs"
+  [{:name "ai-docs"
     :description "Automated code documentation tool."
     :github-link "https://github.com/polyfact/ai-docs"
-    :npm-link "https://www.npmjs.com/package/ai-docs"}])
+    :npm-link "https://www.npmjs.com/package/ai-docs"}
+   {:name "ai-tests"
+    :description "Automated code testing tool."
+    :github-link "https://github.com/lowczarc/ai-tests"
+    :npm-link "https://www.npmjs.com/package/ai-tests"}
+   {:name "ai-logger"
+    :description "Replace your console.log by console.ai"
+    :github-link "https://github.com/kevin-btc/ai-console"
+    :npm-link "https://www.npmjs.com/package/ai-logger"}])
